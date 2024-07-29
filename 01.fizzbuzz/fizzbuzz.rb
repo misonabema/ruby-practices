6646a52c3436b3b1cc6caf6be456e5acdd55fcf7
miso <174323009+misonabema@users.noreply.github.com>
2024-07-29 10:05:21 +0900

    インデント幅とメソッド名を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,13 @@
-def number(n)
-    if n % 15 == 0
-        'FizzBuzz'
-    elsif n % 3 == 0
-        'Fizz'
-    elsif n % 5 == 0
-        'Buzz'
-    else
-        n.to_s
-    end
+def fizzbuzz(n)
+  if n % 15 == 0
+    'FizzBuzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
 end
 
-20.times {|n| puts number(n + 1)}
+20.times {|n| puts fizzbuzz(n + 1)}
