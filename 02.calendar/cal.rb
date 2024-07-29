2463689356906d12ea21ab8e59a6ad1f3eab9a5a
miso <174323009+misonabema@users.noreply.github.com>
2024-07-29 10:41:38 +0900

    インデント幅を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -7,20 +7,20 @@ $assign = ARGV.getopts("m:y:")
 
 #指定がなければ今月を定義
 def month
-    if $assign["m"] == nil
-        Date.today.month 
-    else
-        $assign["m"].to_i
-    end
+  if $assign["m"] == nil
+    Date.today.month 
+  else
+    $assign["m"].to_i
+  end
 end
 
 #指定がなければ今年を定義
 def year
-    if $assign["y"] == nil
-        Date.today.year 
-    else
-        $assign["y"].to_i
-    end
+  if $assign["y"] == nil
+    Date.today.year 
+  else
+    $assign["y"].to_i
+  end
 end
 
 #月と年を表示
@@ -36,17 +36,17 @@ lastday = Date.new(year, month, -1)
 #日付を表示
 print "   " * initialday.wday #１日を曜日によってずらす
 (initialday..lastday).each do |a| 
-    if a.saturday? #土曜で折り返す 
-        if a.day <= 9 #一桁なら手前にスペース入れる
-            puts " #{a.day} "
-        else
-            puts a.day
-        end
+  if a.saturday? #土曜で折り返す 
+    if a.day <= 9 #一桁なら手前にスペース入れる
+      puts " #{a.day} "
     else
-        if a.day <= 9
-            print " #{a.day} "
-        else
-            print "#{a.day} "
-        end
+      puts a.day
     end
+  else
+    if a.day <= 9
+      print " #{a.day} "
+    else
+      print "#{a.day} "
+    end
+  end
 end
