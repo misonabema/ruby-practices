46fb741dfd4d77598286a8a6e8c423a8336a62f1
miso <174323009+misonabema@users.noreply.github.com>
2024-08-04 14:16:04 +0900

    レビューの反映
    消し忘れていたコメントの削除
    month及びyearメソッドをローカル変数に変更
    単語をスネークケースに変更
    ブロック変数をaからdateに変更
    土曜で折り返すif節と1桁を判別するif節を別々に変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,52 +1,29 @@
 #!/usr/bin/env ruby
 
 require 'date'
-#コマンドライン引数の戻り値で表示年月を指定
 require 'optparse'
-$assign = ARGV.getopts("m:y:")
+present = Date.today
 
-#指定がなければ今月を定義
-def month
-  if $assign["m"] == nil
-    Date.today.month 
-  else
-    $assign["m"].to_i
-  end
-end
+params = ARGV.getopts("m:y:")
+month = params["m"]&.to_i || present.month
+year = params["y"]&.to_i || present.year
 
-#指定がなければ今年を定義
-def year
-  if $assign["y"] == nil
-    Date.today.year 
-  else
-    $assign["y"].to_i
-  end
-end
+puts "#{month}月 #{year}".center(20) 
 
-#月と年を表示
-puts "#{month}月 #{year}".center(20) #中央に年月を表示
-
-#曜日を横並びに表示
 puts "日 月 火 水 木 金 土"
 
-#指定した月の初日と末日を取得
-initialday = Date.new(year, month, +1) 
-lastday = Date.new(year, month, -1) 
+initial_day = Date.new(year, month, 1) 
+last_day = Date.new(year, month, -1) 
 
-#日付を表示
-print "   " * initialday.wday #１日を曜日によってずらす
-(initialday..lastday).each do |a| 
-  if a.saturday? #土曜で折り返す 
-    if a.day <= 9 #一桁なら手前にスペース入れる
-      puts " #{a.day} "
-    else
-      puts a.day
-    end
+print "   " * initial_day.wday
+(initial_day..last_day).each do |date| 
+  if date.day <= 9
+    print " #{date.day} "
   else
-    if a.day <= 9
-      print " #{a.day} "
-    else
-      print "#{a.day} "
-    end
+    print "#{date.day} "
+  end
+
+  if date.saturday?
+    puts "\n"
   end
 end
