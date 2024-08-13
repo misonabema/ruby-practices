953a638ae9e742a797cc29af01204018301b8600
miso <174323009+misonabema@users.noreply.github.com>
2024-08-13 16:48:23 +0900

    レビューの反映
    マジックナンバーとなっていた最終フレーム関連の数字に定数を導入
    破壊的メソッドとなっていた部分を削除して点数計算部分で調整
    点数計算部分でsumメソッドを使用し、併せてブロック内部を修正
    次のフレームのスコア取得を説明変数に置き換え

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -7,35 +7,35 @@ require 'rubygems'
 score = ARGV[0]
 scores = score.split(',')
 shots = []
+last_frame_shot = 18
 scores.each do |s|
   if s == 'X'
     shots << 10
-    shots << 0 if shots.count <= 17
+    shots << 0 if shots.count < last_frame_shot
   else
     shots << s.to_i
   end
 end
 
-last_frame = shots.slice!(18..)
 frames = shots.each_slice(2).to_a
-frames.push(last_frame)
 
-point = 0
-frames.each_with_index do |frame, i|
-  point += if i <= 8 # 9フレームまで
-             if frame[0] == 10 # ストライク
-               if frames[i + 1][0] == 10 && i != 8 # ダブル
-                 20 + frames[i + 2][0]
-               else
-                 10 + frames[i + 1][0] + frames[i + 1][1]
-               end
-             elsif frame.sum == 10 # スペア
-               10 + frames[i + 1][0]
-             else
-               frame.sum
-             end
-           else
-             frame.sum # 最終フレーム
-           end
+point = frames.each_with_index.sum do |frame, i|
+  next_frame = frames[i + 1]
+  last_frame = 9
+  if i < last_frame
+    if frame[0] == 10
+      if next_frame[0] == 10 && i != last_frame - 1
+        20 + frames[i + 2][0]
+      else
+        10 + next_frame.sum
+      end
+    elsif frame.sum == 10
+      10 + next_frame[0]
+    else
+      frame.sum
+    end
+  else
+    frame.sum
+  end
 end
 puts point
