f0571c88332a3d0caeaaf8442b852f90e5db77be
miso <174323009+misonabema@users.noreply.github.com>
2024-08-14 09:33:05 +0900

    定数の書き方を修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,17 +1,15 @@
 #!/usr/bin/env ruby
-
 # frozen_string_literal: true
 
-require 'rubygems'
-
+LAST_FRAME_SHOT = 18
+LAST_FRAME = 9
 score = ARGV[0]
 scores = score.split(',')
 shots = []
-last_frame_shot = 18
 scores.each do |s|
   if s == 'X'
     shots << 10
-    shots << 0 if shots.count < last_frame_shot
+    shots << 0 if shots.count < LAST_FRAME_SHOT
   else
     shots << s.to_i
   end
@@ -21,10 +19,9 @@ frames = shots.each_slice(2).to_a
 
 point = frames.each_with_index.sum do |frame, i|
   next_frame = frames[i + 1]
-  last_frame = 9
-  if i < last_frame
+  if i < LAST_FRAME
     if frame[0] == 10
-      if next_frame[0] == 10 && i != last_frame - 1
+      if next_frame[0] == 10 && i != LAST_FRAME - 1
         20 + frames[i + 2][0]
       else
         10 + next_frame.sum
