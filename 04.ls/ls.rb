07ff2a20dd7d8468d25edad25ffc2cc572f05492
miso <174323009+misonabema@users.noreply.github.com>
2024-09-19 17:04:54 +0900

    lsコマンドを作る4

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,14 +13,43 @@ def entry(options)
 end
 
 def files(status)
-  types = { 'file' => '_', 'directory' => 'd', 'link' => 'l' }
-  permission = status.mode.to_s(8)[-3..].chars.map do |n|
-    n.to_i.to_s(2).rjust(3, '0').gsub('1', 'rwx').tr('0', '_')
-  end
-  types[status.ftype] + permission.join
+  file_type = case
+              when status.directory?
+                'd'
+              when status.file?
+                '-'
+              when status.symlink?
+                'l'
+              when status.blockdev?
+                'b'
+              when status.chardev?
+                'c'
+              when status.pipe?
+                'p'
+              when status.socket?
+                's'
+              else
+                '?'
+              end
+  
+  permissions = (0..2).map do |i|
+    shift = i * 3
+    r = (status.mode >> (6 - shift)) & 0o4 > 0 ? 'r' : '-'
+    w = (status.mode >> (6 - shift)) & 0o2 > 0 ? 'w' : '-'
+    x = (status.mode >> (6 - shift)) & 0o1 > 0 ? 'x' : '-'
+    r + w + x
+  end.join
+
+  file_type + permissions
+end
+
+def blocks_total(entries)
+  total_blocks = entries.sum {|file| File.lstat(file).blocks}
+  puts "total #{total_blocks}"
 end
 
 def l_option(entries)
+  blocks_total(entries)
   entries.each do |file|
     status = File.lstat(file)
     mode = files(status)
@@ -29,7 +58,7 @@ def l_option(entries)
     group = Etc.getgrgid(status.gid).name
     size = status.size
     mtime = status.mtime.strftime('%b %d %H:%M')
-    printf("%<mode>s %<nlink>2d %<user>s %<group>s %<size>5d %<mtime>s %<file>s\n",
+    printf("%<mode>s %<nlink>2d %<user>s  %<group>s %<size>5d %<mtime>s %<file>s\n",
            mode:, nlink:, user:, group:, size:, mtime:, file:)
   end
 end
