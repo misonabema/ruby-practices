73487d32fe4c947e53c225e865ccf18f71056180
miso <174323009+misonabema@users.noreply.github.com>
2024-10-08 17:31:22 +0900

    トップレベルに書いていたロジックをmainメソッドで定義
    メソッド名を修正
    メソッドを定義する順を読みやすいように入れ替え
    countメソッド内をfilter_mapメソッドを使う形で再代入のないように修正
    total値手動で表示していたためprint_countsメソッドで表示するよう修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,18 +3,60 @@
 
 require 'optparse'
 
-def option
+def main
+  option = option_hash
+  file_info = read_file_info
+
+  output(option, file_info)
+end
+
+def output(option, file_info)
+  total_counts = nil
+
+  file_info.each do |file|
+    counts = count_content(option, file[:file_content])
+    total_counts ||= Array.new(counts.size, 0)
+
+    print_counts(file[:file_name], counts)
+
+    counts.each_with_index do |count, index|
+      total_counts[index] += count
+    end
+  end
+
+  print_counts('', total_counts, total: true) if ARGV.size > 1
+end
+
+def count_content(option, file_content)
+  counters = {
+    lines: file_content.count("\n"),
+    words: file_content.split(/\s+/).count,
+    chars: file_content.bytesize
+  }
+  option.values.any? ? counters.filter_map { |key, value| value if option[key] } : counters.values
+end
+
+def print_counts(file_name, counts, total: false)
+  format = counts.map { '%8d' }.join('')
+
+  label = total ? 'total' : file_name
+  format += ' %s' unless label.empty?
+
+  printf("#{format}\n", *counts, label)
+end
+
+def option_hash
   options = { lines: false, words: false, chars: false }
-  OptionParser.new do |opts|
+  OptionParser.new do |opt|
     options.each_key do |key|
-      opts.on("-#{key[0]}") { options[key] = true }
+      opt.on("-#{key[0]}") { options[key] = true }
     end
   end.parse!
 
   options
 end
 
-def file_read
+def read_file_info
   if ARGV.empty?
     [{ file_content: ARGF.read, file_name: '' }]
   else
@@ -24,53 +66,4 @@ def file_read
   end
 end
 
-def counter(options, file_content)
-  counters = {
-    lines: file_content.count("\n"),
-    words: file_content.split(/\s+/).count,
-    chars: file_content.bytesize
-  }
-
-  count = []
-  counters.each do |key, value|
-    count << value if options[key]
-  end
-  count = counters.values if count.empty?
-
-  count
-end
-
-def output(file_name, counts)
-  format = counts.map { '%8d' }.join('')
-  format += ' %s' unless file_name.empty?
-
-  if file_name.empty?
-    printf("#{format}\n", *counts)
-  else
-    printf("#{format}\n", *counts, file_name)
-  end
-end
-
-def multi_files(options)
-  file_info = file_read
-  total_counts = nil
-
-  file_info.each do |files|
-    counts = counter(options, files[:file_content])
-    total_counts ||= Array.new(counts.size, 0)
-
-    output(files[:file_name], counts)
-
-    counts.each_with_index do |count, index|
-      total_counts[index] += count
-    end
-  end
-
-  return unless ARGV.size > 1
-
-  format = "#{total_counts.map { '%8d' }.join('')} total\n"
-  printf(format, *total_counts)
-end
-
-options = option
-multi_files(options)
+main
